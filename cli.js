0754afbd099f2f7184313ad2cb783c7dbf3196f3
Francesco Gigli <jaramir@gmail.com>
2018-07-21 22:42:21 +0100

    another one bites the dust

--- cli.js
+++ cli.js
@@ -2,20 +2,19 @@
 
 const path = require('path')
 const os = require('os')
-const minimist = require('minimist')
 const ramda = require('ramda')
 const mtbc = require('./mtbc')
 
-const argv = minimist(process.argv.slice(2))
+const argv = process.argv.slice(2)
 
 const filename = path.join(os.homedir(), '.mtbc.json')
 const history = mtbc.load(filename)
 
-if (argv._.length === 0) {
+if (argv.length === 0) {
   const names = mtbc.getNames(history)
   names.forEach(name => console.log(name))
-} else if (argv._.length === 1) {
-  const name = argv._[0]
+} else if (argv.length === 1) {
+  const name = argv[0]
   const conversation = mtbc.meet(name, new Date())
   mtbc.save(filename, ramda.append(conversation, history))
 } else {
