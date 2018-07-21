6b6a7e1c658fe22076f5ec4a930a74e56b23c58c
Francesco Gigli <jaramir@gmail.com>
2018-07-21 16:46:04 +0100

    functioning command line

--- cli.js
+++ cli.js
@@ -2,10 +2,24 @@
 
 const path = require('path')
 const os = require('os')
+const minimist = require('minimist')
+const ramda = require('ramda')
 const mtbc = require('./mtbc')
+
+const argv = minimist(process.argv.slice(2))
 
 const filename = path.join(os.homedir(), '.mtbc.json')
 const history = mtbc.load(filename)
-const names = mtbc.getNames(history)
 
-names.forEach(name => console.log(name))
+if (argv._.length === 0) {
+  const names = mtbc.getNames(history)
+  names.forEach(name => console.log(name))
+} else if (argv._.length === 1) {
+  const name = argv._[0]
+  const conversation = mtbc.meet(name, new Date())
+  mtbc.save(filename, ramda.append(conversation, history))
+} else {
+  console.log('usage:')
+  console.log('mtbc                        print report')
+  console.log('mtbc <name>                 record conversation')
+}
