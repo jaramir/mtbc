1c6d6f085415b4e7bbc45fbe7f0edd2e27f63c21
Francesco Gigli <jaramir@gmail.com>
2018-09-08 01:29:01 +0100

    very raw autocompletion

--- cli.js
+++ cli.js
@@ -5,22 +5,44 @@ const os = require('os')
 const ramda = require('ramda')
 const mtbc = require('./mtbc')
 const inquirer = require('inquirer')
+const autocompletePrompt = require('inquirer-autocomplete-prompt')
+const fuzzy = require('fuzzy')
+const filename = path.join(os.homedir(), '.mtbc.json')
 
-function recordConversation (who) {
-  const filename = path.join(os.homedir(), '.mtbc.json')
+inquirer.registerPrompt('autocomplete', autocompletePrompt)
+
+const source = (answersSoFar, input) => new Promise(resolve => {
   const history = mtbc.load(filename)
-  const conversation = mtbc.meet(who, new Date())
-  mtbc.save(filename, ramda.append(conversation, history))
-}
+  const names = mtbc.getNames(history)
+
+  if (!input) {
+    resolve(names)
+    return
+  }
+
+  const matches = fuzzy
+    .filter(input, names)
+    .map(ramda.prop('string'))
+
+  if (matches.length === 0) {
+    resolve([input])
+    return
+  }
+
+  resolve(matches)
+})
 
 inquirer
   .prompt({
-    type: 'input',
+    type: 'autocomplete',
     name: 'who',
-    message: 'Who have you had a conversation with?'
+    message: 'Who have you had a conversation with?',
+    source
   })
   .then(answers => {
     if (answers.who) {
-      recordConversation(answers.who)
+      const history = mtbc.load(filename)
+      const conversation = mtbc.meet(answers.who, new Date())
+      mtbc.save(filename, ramda.append(conversation, history))
     }
   })
